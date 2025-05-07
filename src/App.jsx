eebbe8cf8fec103b831adec95510c79d95892df1
Karim Bonda <karimbonda@outlook.com>
2025-05-07 23:09:22 +0300

    fix import NavBar.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import ScrollZoom from './pages/ScrollZoom';
-import NavBar from './components/NavBar';
-import Footer from './components/Footer';
+import NavBar from './components/NavBar.jsx';
+import Footer from './components/Footer.jsx';
 
 function App() {
   return (
